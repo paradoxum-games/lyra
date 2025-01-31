4d058ad937517f8243ae50fd533b1a8a56e4a0b3
Six <23470032+6ixfalls@users.noreply.github.com>
2025-01-30 19:26:43 -0800

    feat: default export fix

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,55 +1,52 @@
-import { MigrationStep } from "./store";
+import { MigrationStep as TMigrationStep } from "./store";
 import { PlayerStoreConfig, PlayerStore } from "./playerstore";
 import { LogLevel } from "./log";
 
-declare namespace Lyra {
-    export const MigrationStep: {
-        /**
-         * Adds fields to unmigrated data.
-         *
-         * @param name The name of the migration step (Must be unique and cannot be changed later)
-         * @param fields The fields to add to the data
-         */
-        addFields<Arg, Return, NewFields extends object>(
-            name: string,
-            fields: NewFields
-        ): MigrationStep<Arg, Return & NewFields>;
-        /**
-         * Transforms the data using a function.
-         *
-         * @param name The name of the migration step (Must be unique and cannot be changed later)
-         * @param transformer The function to transform the data
-         */
-        transform<Arg, Return>(
-            name: string,
-            transformer: (data: Arg) => Return
-        ): MigrationStep<Arg, Return>;
-    };
-
+export namespace MigrationStep {
     /**
-     * Creates a player store.
+     * Adds fields to unmigrated data.
      *
-     * @param config The configuration for the player store.
+     * @param name The name of the migration step (Must be unique and cannot be changed later)
+     * @param fields The fields to add to the data
      */
-    export function createPlayerStore<Schema extends object>(
-        config: PlayerStoreConfig<Schema>
-    ): PlayerStore<Schema>;
-
+    export function addFields<Arg, Return, NewFields extends object>(
+        name: string,
+        fields: NewFields
+    ): TMigrationStep<Arg, Return & NewFields>;
     /**
-     * Sets the log level for the library.
+     * Transforms the data using a function.
      *
-     * @param level The log level to set.
+     * @param name The name of the migration step (Must be unique and cannot be changed later)
+     * @param transformer The function to transform the data
      */
-    export function setLogLevel(level: LogLevel): void;
+    export function transform<Arg, Return>(
+        name: string,
+        transformer: (data: Arg) => Return
+    ): TMigrationStep<Arg, Return>;
 }
+
+/**
+ * Creates a player store.
+ *
+ * @param config The configuration for the player store.
+ */
+export function createPlayerStore<Schema extends object>(
+    config: PlayerStoreConfig<Schema>
+): PlayerStore<Schema>;
+
+/**
+ * Sets the log level for the library.
+ *
+ * @param level The log level to set.
+ */
+export function setLogLevel(level: LogLevel): void;
 
 export type { LogLevel, LogMessage } from "./log";
 export { PlayerStore } from "./playerstore";
 export type { PlayerStoreConfig } from "./playerstore";
 export { Store } from "./store";
-export type { StoreConfig, MigrationStep } from "./store";
+export type { StoreConfig, MigrationStep as TMigrationStep } from "./store";
 export {
     DataStoreService as MockDataStoreService,
     MemoryStoreService as MockMemoryStoreService,
 } from "./mock";
-export default Lyra;
